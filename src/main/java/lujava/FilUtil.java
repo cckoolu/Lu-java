eb6246057b27835db789ade07be0489de1b1050f
cckoolu <cckoolu@gmail.com>
2021-06-10 10:30:04 +0800

    添加inputstream输出本地文件方法

---
+++ src/main/java/lujava/FilUtil.java
@@ -0,0 +1,26 @@
+package lujava;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@SuppressWarnings(value = "unused")
+public class FilUtil {
+    /**
+     * @param destination 保存文件位置
+     * @param inputStream 文件输入流
+     */
+
+    public static void writeToLocal(String destination, InputStream inputStream)
+            throws IOException {
+        int index;
+        byte[] bytes = new byte[inputStream.available()];
+        FileOutputStream downloadFile = new FileOutputStream(destination);
+        while ((index = inputStream.read(bytes)) != -1) {
+            downloadFile.write(bytes, 0, index);
+            downloadFile.flush();
+        }
+        downloadFile.close();
+        inputStream.close();
+    }
+}
